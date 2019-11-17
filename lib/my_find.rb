bf78bf100a4c4bde3152858a58fb4c8c3c803116
Ivana <ivana_pile@yahoo.com>
2019-11-16 21:43:38 -0500

    Done.

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -1,5 +1,25 @@
 require 'pry'
-
 def my_find(collection)
+    i = 0
+    while i < collection.length
+      i = i + 1
+    end
+  end
 
-end
+  def my_find(collection)
+    i = 0
+    while i < 100
+      yield(1)
+        i = i + 1
+    end
+  end
+
+  def my_find(collection)
+    i = 0
+    while i < collection.length
+      if yield(collection[i])
+        return collection[i]
+      end
+      i = i + 1
+    end
+  end
